58bd1025f744115d1174fe8ec79d9e93f5c36a9d
wilcrofter <bill@crofter.net>
2016-07-27 18:16:46 -0400

    begin Buchberger

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -6,6 +6,26 @@ function S_poly(A::Multinomial, B::Multinomial)
         A[end].coefficient*x^(tmp-B[end].exponent)*B
 end
 
+"""
+Does monomial a divide monomial b?
+"""
+function divides(a::Monomial, b::Monomial)
+   !isapprox(a.coefficient, 0) &&
+       all(a.exponent .<= b.exponent) &&
+       a.indeterminate == b.indeterminate
+end
+
+"""
+Quotient of b and a. Assumes divisability
+"""
+function /(b::monomial, a::monomial)
+    Monomial(b.indeterminate,
+             b.exponent-a.exponent,
+             b.coefficient/a.coefficient)
+end
+
+
+
 function demo_alg()
     @show x = Indeterminate(3)
     println()
@@ -15,3 +35,12 @@ function demo_alg()
     println()
     @show S_poly(A,B)
 end
+
+#=
+Start with G = F .
+For any pair of polynomials f1 , f2 in G:
+  Compute the S✁polynomial of f1 , f2
+  and reduce it to a reduced form h w.r.t. G.
+  If h is not 0, consider the next pair.
+  If h ✑ 0, add h to G and iterate.
+=#
